715e47cd6ada05cca8fb9cad02dae4a7c9cd1d4d
Camilo Henriquez <chenriquez@cloudtecnologias.com>
2020-08-17 09:38:23 -0400

    add file service2

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/dataServices/FileService.java
@@ -0,0 +1,23 @@
+package com.udacity.jwdnd.course1.cloudstorage.services.dataServices;
+
+import com.udacity.jwdnd.course1.cloudstorage.data.File;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FileService {
+
+    @Autowired
+    private FileMapper fileMapper;
+
+    public File getFileUpload(Long fileId){ return fileMapper.getFile(fileId); }
+
+    public void insertFile(File file){   fileMapper.insertFile(file); }
+
+    public void deleteFile(Long fileId){ fileMapper.deleteFile(fileId);}
+
+    public List<File> filesUpload(Long userId){ return fileMapper.getFilesByUserId(userId);  }
+}
